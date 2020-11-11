4c385b0702a8e5319a9e07bd14855dae41051bb2
Aaron Doucett <aaron@doucett.net>
2020-11-10 19:47:24 -0500

    Update app.js
    yes

--- src/app.js
+++ src/app.js
@@ -111,7 +111,7 @@ function sanitizeHTML(strings) {
 function initMap() {
   // Create the map.
   const map = new google.maps.Map(document.getElementById('map'), {
-    mapID: "21c7a039bcddf093"
+    mapID: "21c7a039bcddf093",
 	zoom: 7,
     center: {lat: 52.632469, lng: -1.689423}
     // styles: mapStyle,
