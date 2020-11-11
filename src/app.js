4a0cb60de52943d486fb4f8ad4ac25343b1acaac
Aaron Doucett <aaron@doucett.net>
2020-11-10 19:45:49 -0500

    update

--- src/app.js
+++ src/app.js
@@ -111,9 +111,10 @@ function sanitizeHTML(strings) {
 function initMap() {
   // Create the map.
   const map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 7,
-    center: {lat: 52.632469, lng: -1.689423},
-    styles: mapStyle,
+    mapID: "21c7a039bcddf093"
+	zoom: 7,
+    center: {lat: 52.632469, lng: -1.689423}
+    // styles: mapStyle,
   });
 
   // Load the stores GeoJSON onto the map.
