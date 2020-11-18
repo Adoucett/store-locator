147b253bba5ad0d021723f02edf5ba3c6c0ea1a2
Aaron Doucett <aaron@doucett.net>
2020-11-18 11:08:07 -0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -211,7 +211,7 @@ function initMap() {
     // Recenter the map to the selected address
     originLocation = place.geometry.location;
     map.setCenter(originLocation);
-    map.setZoom(12);
+    map.setZoom(14);
     console.log(place);
 
     originMarker.setPosition(originLocation);
